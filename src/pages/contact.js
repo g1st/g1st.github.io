e37bb462b97413116bf487fac1f3301caeae6365
g1st <gintstan@gmail.com>
2020-03-11 17:05:56 +0000

    back to square 1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,78 +1,75 @@
-import React from "react";
-import { navigate } from "gatsby";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+import SEO from "../components/seo";
+import { Button } from "../styles/mixins";
 
-export default function Contact() {
-  const [state, setState] = React.useState({});
+const Container = styled.div`
+  max-width: 42rem;
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
 
-  const handleChange = e => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
+const Heading = styled.h2`
+  font-size: 3rem;
+  text-align: center;
+`;
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
-  return (
-    <>
-      <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out:{" "}
-            <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
-  );
-}
+const Text = styled.p`
+  margin-top: 3rem;
+`;
+
+const Input = styled.input`
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 3em;
+  border-radius: 3px;
+  border: none;
+`;
+
+const Message = styled.textarea`
+  border: none;
+  border-radius: 3px;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 3em;
+`;
+
+const Submit = styled(Button)`
+  max-width: 12em;
+`;
+
+const Contact = () => (
+  <Container>
+    <SEO title="Contact" />
+    <Heading>Contact</Heading>
+    <Text>
+      Have a question or looking to work together?Feel free to contact me!
+    </Text>
+    <Form
+      name="contact2"
+      method="post"
+      action="/thanks"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact2" />
+      <input type="hidden" name="bot-field" />
+      <Input name="email" type="email" placeholder="Email" required />
+      <Message
+        name="message"
+        placeholder="Message"
+        rows="10"
+        cols="50"
+        minLength="30"
+        required
+      />
+      <Submit type="submit">Send Message</Submit>
+    </Form>
+  </Container>
+);
+
+export default Contact;
