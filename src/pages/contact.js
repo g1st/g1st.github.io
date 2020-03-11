7d13d208c84ac6192dfbac3ba94be2a648909ca4
g1st <gintstan@gmail.com>
2020-03-11 15:11:19 +0000

    Control the form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { navigate } from 'gatsby';
 import styled from "styled-components";
 
+import SEO from '../components/seo';
 import { Button } from "../styles/mixins";
 
 const Container = styled.div`
@@ -41,26 +43,65 @@ const Submit = styled(Button)`
   max-width: 12em;
 `;
 
-const Contact = () => (
-  <Container>
-    <Heading>Contact</Heading>
-    <Text>
-      Have a question or looking to work together? Feel free to contact me!
-    </Text>
-    <Form name="contact" method="POST" action="/" netlify>
-      <input type="hidden" name="form-name" value="contact" />
-      <Input name="email" type="email" placeholder="Email" required />
-      <Message
-        name="message"
-        placeholder="Message"
-        rows="10"
-        cols="50"
-        minLength="30"
-        required
-      />
-      <Submit type="submit">Send Message</Submit>
-    </Form>
-  </Container>
-);
+const encode = data =>
+  Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+
+const Contact = () => {
+  const [state, setState] = React.useState({});
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
+  return (
+    <Container>
+      <SEO title="Contact">
+      <Heading>Contact</Heading>
+      <Text>
+        Have a question or looking to work together?Feel free to contact me!
+      </Text>
+      <Form name="contact" method="POST" action="thanks" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <Input
+          onChange={handleChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Message
+          onChange={handleChange}
+          name="message"
+          placeholder="Message"
+          rows="10"
+          cols="50"
+          minLength="30"
+          required
+        />
+        <Submit type="submit" onSubmit={handleSubmit}>
+          Send Message
+        </Submit>
+      </Form>
+    </Container>
+  );
+};
 
 export default Contact;
