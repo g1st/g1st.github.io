a50b4fff324a99b1029981baf2db30051db2a93c
g1st <gintstan@gmail.com>
2020-03-11 16:02:01 +0000

    straight copy paste to check if netlify forms can even work

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,75 +1,83 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React from "react";
+import { navigate } from "gatsby";
 
-import SEO from "../components/seo";
-import { Button } from "../styles/mixins";
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-const Container = styled.div`
-  max-width: 42rem;
-  margin: 0 auto;
-  padding: 0 2rem;
-`;
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-const Heading = styled.h2`
-  font-size: 3rem;
-  text-align: center;
-`;
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
-const Text = styled.p`
-  margin-top: 3rem;
-`;
-
-const Input = styled.input`
-  padding: 0.75rem 1.25rem;
-  margin-bottom: 3em;
-  border-radius: 3px;
-  border: none;
-`;
-
-const Message = styled.textarea`
-  border: none;
-  border-radius: 3px;
-  padding: 0.75rem 1.25rem;
-  margin-bottom: 3em;
-`;
-
-const Submit = styled(Button)`
-  max-width: 12em;
-`;
-
-const Contact = () => (
-  <Container>
-    <SEO title="Contact" />
-    <Heading>Contact</Heading>
-    <Text>
-      Have a question or looking to work together?Feel free to contact me!
-    </Text>
-    <form
-      name="contact"
-      method="post"
-      action="/thanks"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <input type="hidden" name="bot-field" />
-      <input name="email" type="email" placeholder="Email" required />
-      <textarea
-        name="message"
-        placeholder="Message"
-        rows="10"
-        cols="50"
-        minLength="30"
-        required
-      />
-      <Submit type="submit">Send Message</Submit>
-    </form>
-  </Container>
-);
-
-export default Contact;
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
