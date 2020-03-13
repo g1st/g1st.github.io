27b07ee46d537af81b0745876cff4068f4e1088b
g1st <gintstan@gmail.com>
2020-03-13 16:09:28 +0000

    Add accompanying font

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Open Sans\:400,600`],
+        fonts: [`Open Sans\:400,600`, `Merriweather\:400`],
         display: `swap`,
       },
     },
--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -11,13 +11,17 @@ export const GlobalStyle = createGlobalStyle`
     transition: all 0.15s linear;
     color: ${({ theme }) => theme.colors.text};
   }
-
+  
   p {
     line-height: 1.15;
   }
-
+  
   a {
     color:  ${({ theme }) => theme.colors.text};
     text-decoration: none;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Merriweather', 'Open Sans', Arial, Helvetica, sans-serif;
   }
 `;
