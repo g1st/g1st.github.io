fe5e38773fdd683b46291097ec87184a083fbe01
g1st <gintstan@gmail.com>
2020-03-25 18:48:59 +0000

    Add mixin

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -45,3 +45,26 @@ export const AnchorLink = styled.a`
     background: ${({ theme }) => theme.colors.tertiary};
   }
 `;
+
+export const TextLink = styled.a`
+  position: relative;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.tertiary};
+
+  :before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: ${({ theme }) => theme.colors.tertiary};
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: all 0.15s ease-in-out;
+  }
+
+  :hover:before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+`;
