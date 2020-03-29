314c1affdfcd3b3fb71e35a53abba04beff5e8d4
g1st <gintstan@gmail.com>
2020-03-29 12:17:07 +0100

    Add a11y tests

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -3,20 +3,35 @@
 describe("Accessibility tests", () => {
   beforeEach(() => {
     cy.visit("/")
-      .get("main")
+      .get("#___gatsby")
       .injectAxe();
   });
   it("Has no detectable accessibility violations on load", () => {
     cy.checkA11y();
   });
   it("Navigates to /blog and checks for accessibility violations", () => {
-    cy.findByText(/blog/i)
-      .click({ force: true }) // text is visible only on hover
+    cy.findByLabelText(/View blog page/)
+      .click()
       .checkA11y();
   });
   it("Navigates to /first-post and checks for accessibility violations", () => {
     cy.findByLabelText(/View blog page/).click();
     cy.findByText(/First Post/i)
+      .click()
+      .checkA11y();
+  });
+  it("Navigates to /projects and checks for accessibility violations", () => {
+    cy.findByLabelText(/View projects page/)
+      .click()
+      .checkA11y();
+  });
+  it("Navigates to /contact and checks for accessibility violations", () => {
+    cy.findByLabelText(/View contact page/)
+      .click()
+      .checkA11y();
+  });
+  it("Navigates to /contact and checks for accessibility violations", () => {
+    cy.findByTitle(/Activate/)
       .click()
       .checkA11y();
   });
