09e89cfdb3ebc7ae3bef65d17a87dacba1833565
g1st <gintstan@gmail.com>
2020-08-11 09:38:09 +0100

    Fix lint issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
+import { wrapRootElement as wrap } from "./rootWrapper";
 require("typeface-merriweather");
 require("typeface-open-sans");
-import { wrapRootElement as wrap } from "./rootWrapper";
 
 export const wrapRootElement = wrap;
